bc80a9d28586d29a808859119461ad686343d4a3
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2024-01-28 16:24:55 +0300

    Add models and migrate them to DB.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.db.models import *
 
-# Create your models here.
+class Tag(Model):
+    name = CharField(max_length=16, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Note(Model):
+    title = CharField(max_length=256)
+    content = TextField(blank=True)
+    created_at = DateTimeField(auto_now_add=True)
+    tags = ManyToManyField(Tag)
+
+    def __str__(self) -> str:
+        return self.title
+
